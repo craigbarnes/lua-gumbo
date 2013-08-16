be809089bbc5556c97b44f3f407d137f9df44ead
Craig Barnes <cr@igbarn.es>
2013-08-16 17:08:57 +0100

    Add stub build_table function

--- lgumbo.c
+++ lgumbo.c
@@ -2,11 +2,44 @@
 #include <lauxlib.h>
 #include <gumbo.h>
 
+static void build_table(lua_State *L, GumboNode* node) {
+    switch (node->type) {
+    case GUMBO_NODE_ELEMENT:
+        lua_newtable(L);
+        lua_pushstring(L, gumbo_normalized_tagname(node->v.element.tag));
+        lua_setfield(L, -2, "tag");
+        GumboVector* children = &node->v.element.children;
+        for (int i = 0; i < children->length; ++i) {
+            build_table(L, children->data[i]);
+        }
+        break;
+
+    case GUMBO_NODE_DOCUMENT:
+        break;
+
+    case GUMBO_NODE_TEXT:
+    case GUMBO_NODE_CDATA:
+    case GUMBO_NODE_COMMENT:
+    case GUMBO_NODE_WHITESPACE:
+        lua_pushstring(L, node->v.text.text);
+        lua_rawseti(L, -2, node->index_within_parent);
+        printf("%s\n", node->v.text.text);
+        break;
+    }
+
+//    lua_pop(L, 1);
+}
+
 static int parse(lua_State *L) {
-    GumboOutput* output = gumbo_parse("test");
-    // Use output->root
+    size_t len;
+    const char *input;
+    GumboOutput *output;
+
+    input = luaL_checklstring(L, 1, &len);
+    output = gumbo_parse_with_options(&kGumboDefaultOptions, input, len);
+    build_table(L, output->root);
     gumbo_destroy_output(&kGumboDefaultOptions, output);
-    return 0;
+    return 1;
 }
 
 static const luaL_reg R[] = {
--- test.lua
+++ test.lua
@@ -1,9 +1,11 @@
-package.path = ""
 package.cpath = "./?.so"
-
 local gumbo = require "gumbo"
+local serpent = require "serpent"
 
 local output = gumbo.parse [[
     <title>Test Document</title>
     <h1>Test heading</h1>
+    <a href=foobar.html>Quux</a>
 ]]
+
+print(serpent.block(output))
