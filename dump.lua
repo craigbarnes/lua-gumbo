6c06b9b78a1c12613d63fb79db236c24b9a4e9f9
Craig Barnes <cr@igbarn.es>
2013-08-19 20:05:00 +0100

    Improve dump.lua

--- dump.lua
+++ dump.lua
@@ -7,18 +7,25 @@ local document = gumbo.parse [[
     <p><a href=foobar.html><strong>Bold Text</strong> etc.</a></p>
 ]]
 
-local depth = 0
 
+local function write(text, depth, quoted)
+    local indent = string.rep(" ", depth*4)
+    local text = text:match("^%s*(.*)")
+    local format = (quoted and #text > 1) and '%s"%s"\n' or '%s%s\n'
+    io.write(string.format(format, indent, text))
+end
+
+local depth = 0
 local function dump(node)
     if node.tag then
-        io.write(string.format("%s%s\n", string.rep(" ", depth*4), node.tag))
+        write(node.tag, depth)
         depth = depth + 1
         for i = 1, #node do
            dump(node[i])
         end
         depth = depth - 1
     else
-        io.write(string.format("%s%s\n", string.rep(" ", depth*4), node))
+        write(node, depth, true)
     end
 end
 
