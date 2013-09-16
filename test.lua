4b868674f96a9272bda2598ad2607928a57e5919
Craig Barnes <cr@igbarn.es>
2013-09-16 21:31:31 +0100

    Clean up test.lua

--- test.lua
+++ test.lua
@@ -1,20 +1,23 @@
 package.cpath = "./?.so"
 local gumbo = require "gumbo"
-
 local document = assert(gumbo.parse_file "test.html")
-local root = document.root
+local root = assert(document.root)
+local head, body = root[1], root[2]
 
 assert(root.tag == "html")
-assert(root[1][1].tag == "title")
-assert(root[1][1][1] == "Test Document")
-assert(root[2][1].tag == "h1")
-assert(root[2][1][1] == "Test Heading")
-assert(root[2][2].tag == "p")
-assert(root[2][2][1].attr.href == "foobar.html")
-assert(root[2][3].tag == "invalid")
-assert(root[2][3].attr.foo == "bar")
-assert(root[2][3][1] == "abc")
-assert(root[2][4].attr.class == "empty")
-assert(root[2][4].length == 0)
+assert(head.tag == "head")
+assert(body.tag == "body")
+assert(head[1].tag == "title")
+assert(head[1][1] == "Test Document")
+assert(body[1].tag == "h1")
+assert(body[1][1] == "Test Heading")
+assert(body[2].tag == "p")
+assert(body[2][1].attr.href == "foobar.html")
+assert(body[3].tag == "invalid")
+assert(body[3].attr.foo == "bar")
+assert(body[3][1] == "abc")
+assert(body[4].attr.class == "empty")
+assert(body[4].length == 0)
 
+assert(gumbo.parse("<h1>Hello</h1>").root[2][1][1] == "Hello")
 assert(not gumbo.parse_file "non-existent-file")
