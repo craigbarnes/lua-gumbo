f8b88af698d1a773bbb142f6f0c6c09dee332918
Craig Barnes <cr@igbarn.es>
2013-09-17 08:46:54 +0100

    Add dump.lua

---
+++ examples/dump.lua
@@ -0,0 +1,9 @@
+package.cpath = "./?.so;../?.so"
+local gumbo = require "gumbo"
+local serpent = require "serpent"
+
+local filename = ...
+assert(filename, "A filename argument is required")
+
+local document = assert(gumbo.parse_file(filename))
+print(serpent.block(document, {comment = false}))
