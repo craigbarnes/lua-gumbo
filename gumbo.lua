f969c6bd041258b028041cb07bca0be3657db7f3
Craig Barnes <cr@igbarn.es>
2013-11-15 20:09:03 +0000

    Add license header to gumbo.lua

--- gumbo.lua
+++ gumbo.lua
@@ -1,3 +1,20 @@
+--[[
+ LuaJIT FFI/luaffi bindings for the Gumbo HTML5 parsing library.
+ Copyright (c) 2013 Craig Barnes
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+]]
+
 local have_ffi, ffi = pcall(require, "ffi")
 
 if not have_ffi then -- load the C module instead
