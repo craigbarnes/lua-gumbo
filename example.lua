7417a046fae02c56ec9fd41b968e7e599cd77804
Craig Barnes <cr@igbarn.es>
2013-11-22 02:33:10 +0000

    Make example.lua serialize as HTML

--- example.lua
+++ example.lua
@@ -15,17 +15,35 @@ local document = gumbo.parse [[
 </html>
 ]]
 
-local function dump(node)
+local void = {
+    area = true, base = true, br = true, col = true, command = true,
+    embed = true, hr = true, img = true, input = true, keygen = true,
+    link = true, meta = true, param = true, source = true, track = true,
+    wbr = true,
+}
+
+local function printf(...)
+    io.write(string.format(...))
+end
+
+local function serialize(node)
     if node.type == "element" then
-        io.write(string.format('%s%s\n', indent, node.tag))
+        printf('%s<%s', indent, node.tag)
+        for name, value in pairs(node.attr or {}) do
+            printf(' %s="%s"', name, value)
+        end
+        printf(">\n")
         indent = indent .. "    "
         for i = 1, #node do
-           dump(node[i])
+            serialize(node[i])
         end
         indent = indent:sub(1, -5)
+        if not void[node.tag] then
+            printf("%s</%s>\n", indent, node.tag)
+        end
     elseif node.type == "text" then
-        io.write(string.format('%s"%s"\n', indent, node.text))
+        printf('%s%s\n', indent, node.text)
     end
 end
 
-dump(document.root)
+serialize(document.root)
