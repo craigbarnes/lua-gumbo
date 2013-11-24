7548c29365ecbd52f7d4d27bccb2191828f514be
Craig Barnes <cr@igbarn.es>
2013-11-24 22:15:41 +0000

    Memoize indent strings generated during serialization

--- gumbo/serialize.lua
+++ gumbo/serialize.lua
@@ -1,35 +1,64 @@
 -- Work in progress -- Do not use this yet --
 
+-- Set of void elements
+-- whatwg.org/specs/web-apps/current-work/multipage/syntax.html#void-elements
 local void = {
-    area = true, base = true, br = true, col = true, command = true,
-    embed = true, hr = true, img = true, input = true, keygen = true,
-    link = true, meta = true, param = true, source = true, track = true,
-    wbr = true,
+    area = true,
+    base = true,
+    br = true,
+    col = true,
+    embed = true,
+    hr = true,
+    img = true,
+    input = true,
+    keygen = true,
+    link = true,
+    menuitem = true,
+    meta = true,
+    param = true,
+    source = true,
+    track = true,
+    wbr = true
 }
 
 local function printf(...)
     io.write(string.format(...))
 end
 
-local indent = ""
+-- Generates a string of spaces for a given level of indentation.
+-- The generated strings are memoized in a table, since once a level is
+-- reached, indents at that level are almost certain to be used again.
+local indent = setmetatable({[0] = "", [1] = "    "}, {
+    __index = function(self, i)
+        self[i] = string.rep(self[1], i)
+        return self[i]
+    end
+})
+
+level = 0
 
 local function serialize(node)
     if node.type == "element" then
-        printf('%s<%s', indent, node.tag)
+        -- Add start tag and attributes
+        printf('%s<%s', indent[level], node.tag)
         for name, value in pairs(node.attr or {}) do
             printf(' %s="%s"', name, value)
         end
         printf(">\n")
-        indent = indent .. "    "
+
+        -- Recurse into child nodes
+        level = level + 1
         for i = 1, #node do
             serialize(node[i])
         end
-        indent = indent:sub(1, -5)
+        level = level - 1
+
+        -- Add end tag if not a void element
         if not void[node.tag] then
-            printf("%s</%s>\n", indent, node.tag)
+            printf("%s</%s>\n", indent[level], node.tag)
         end
     elseif node.type == "text" then
-        printf('%s%s\n', indent, node.text)
+        printf('%s%s\n', indent[level], node.text)
     end
 end
 
