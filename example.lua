7dd197ba023c637b1ebdfabb832f8da2670500fe
Craig Barnes <cr@igbarn.es>
2013-12-06 22:01:26 +0000

    Clean up example.lua

--- example.lua
+++ example.lua
@@ -19,17 +19,8 @@ local function check(cond, msg)
         local fmt = "Error: %s\nTry '%s help' for more information\n"
         io.stderr:write(string.format(fmt, msg, arg[0]))
         os.exit(1)
-    end
-end
-
-local function parse_file(filename)
-    local file, err = io.open(filename)
-    if file then
-        local text = file:read("*a")
-        file:close()
-        return gumbo.parse(text)
     else
-        check(false, err)
+        return cond
     end
 end
 
@@ -39,22 +30,21 @@ local action = {
     end,
     html = function(filename)
         local serialize = require("gumbo.serialize").to_html
-        local document = parse_file(filename)
+        local document = check(gumbo.parse_file(filename))
         io.stdout:write(serialize(document))
     end,
     table = function(filename)
         local serialize = require("gumbo.serialize").to_table
-        local document = parse_file(filename)
+        local document = check(gumbo.parse_file(filename))
         io.stdout:write(serialize(document))
     end,
     bench = function(filename)
-        parse_file(filename)
+        check(gumbo.parse_file(filename))
     end
 }
 
-local command = arg[1]
+local command = check(arg[1], "no command specified")
 local filename = arg[2]
-check(command, "no command specified")
 check(action[command], "invalid command")
 check(filename or command == "help", "no filename specified")
 action[command](filename)
