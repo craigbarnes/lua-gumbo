d682a333247d6ad70a89e25dd2d8e4dca1516b61
Craig Barnes <Cr@igBarn.es>
2014-02-07 18:57:29 +0000

    Remove all uses of string.format() from gumbo/serialize/table.lua

--- gumbo/serialize/table.lua
+++ gumbo/serialize/table.lua
@@ -1,6 +1,5 @@
 local Buffer = require "gumbo.buffer"
 local Indent = require "gumbo.indent"
-local fmt = string.format
 
 local parse_flags_fields = {
     -- Serialized in this order:
@@ -37,75 +36,71 @@ end
 local function to_table(node, buffer, indent_width)
     local buf = buffer or Buffer()
     local indent = Indent(indent_width)
-    local sfmt = '%s%s = "%s",\n'
-    local nfmt = "%s%s = %d,\n"
-    local bfmt = '%s%s = %s,\n'
 
     -- TODO: omit trailing commas where not required
     local function serialize(node, level)
         if node.type == "element" then
-            buf:write(fmt("%s{\n", indent[level]))
+            buf:write(indent[level], '{\n')
             local i1, i2 = indent[level+1], indent[level+2]
-            buf:write(fmt(sfmt, i1, "type", "element"))
-            buf:write(fmt(sfmt, i1, "tag", node.tag))
-            buf:write(fmt(sfmt, i1, "tag_namespace", node.tag_namespace))
-            buf:write(fmt(nfmt, i1, "line", node.line))
-            buf:write(fmt(nfmt, i1, "column", node.column))
-            buf:write(fmt(nfmt, i1, "offset", node.offset))
+            buf:write(i1, 'type = "element",\n')
+            buf:write(i1, 'tag = "', node.tag, '",\n')
+            buf:write(i1, 'tag_namespace = "', node.tag_namespace, '",\n')
+            buf:write(i1, 'line = ', node.line, ',\n')
+            buf:write(i1, 'column = ', node.column, ',\n')
+            buf:write(i1, 'offset = ', node.offset, ',\n')
 
             if #node.attr > 0 then
                 local i3 = indent[level+3]
                 local tmp = Buffer()
-                buf:write(fmt("%sattr = {\n", i1))
+                buf:write(i1, 'attr = {\n')
                 for i, name, val, ns, line, col, offset in node:attr_iter() do
-                    buf:write(fmt(sfmt, i2, escape_key(name), escape(val)))
-                    tmp:write(fmt("%s{\n", i2))
-                    tmp:write(fmt(sfmt, i3, "name", escape(name)))
-                    tmp:write(fmt(sfmt, i3, "value", escape(val)))
-                    if ns then tmp:write(fmt(sfmt, i3, "namespace", ns)) end
-                    tmp:write(fmt(nfmt, i3, "line", line))
-                    tmp:write(fmt(nfmt, i3, "column", col))
-                    tmp:write(fmt("%s%s = %d\n", i3, "offset", offset))
-                    tmp:write(fmt("%s},\n", i2))
+                    buf:write(i2, escape_key(name), ' = "', escape(val), '",\n')
+                    tmp:write(i2, "{\n")
+                    tmp:write(i3, 'name = "', escape(name), '",\n')
+                    tmp:write(i3, 'value = "', escape(val), '",\n')
+                    if ns then tmp:write(i3, 'namespace = "', ns, '",\n') end
+                    tmp:write(i3, 'line = ', line, ',\n')
+                    tmp:write(i3, 'column = ', col, ',\n')
+                    tmp:write(i3, 'offset = ', offset, '\n')
+                    tmp:write(i2, '},\n')
                 end
-                buf:write(tostring(tmp))
-                buf:write(fmt("%s},\n", i1))
+                buf:write(tostring(tmp), i1, '},\n')
             end
 
             if node.parse_flags then
-                buf:write(fmt("%sparse_flags = {\n", i1))
+                buf:write(i1, 'parse_flags = {\n')
                 for i = 1, #parse_flags_fields do
                     local field = parse_flags_fields[i]
                     local value = node.parse_flags[field]
                     if value then
-                        buf:write(fmt(bfmt, i2, field, value))
+                        buf:write(i2, field, " = ", tostring(value), ",\n")
                     end
                 end
-                buf:write(fmt("%s},\n", i1))
+                buf:write(i1, '},\n')
             end
             for i = 1, #node do
                 serialize(node[i], level + 1)
             end
-            buf:write(fmt("%s},\n", indent[level]))
+            buf:write(indent[level], '},\n')
         elseif node.text then
             local i1, i2 = indent[level], indent[level+1]
-            buf:write(fmt("%s{\n", i1))
-            buf:write(fmt(sfmt, i2, "type", node.type))
-            buf:write(fmt(sfmt, i2, "text", escape(node.text)))
-            buf:write(fmt(nfmt, i2, "line", node.line))
-            buf:write(fmt(nfmt, i2, "column", node.column))
-            buf:write(fmt(nfmt, i2, "offset", node.offset))
-            buf:write(fmt("%s},\n", i1))
+            buf:write(i1, '{\n')
+            buf:write(i2, 'type = "', node.type, '",\n')
+            buf:write(i2, 'text = "', escape(node.text), '",\n')
+            buf:write(i2, 'line = ', node.line, ',\n')
+            buf:write(i2, 'column = ', node.column, ',\n')
+            buf:write(i2, 'offset = ', node.offset, ',\n')
+            buf:write(i1, '},\n')
         elseif node.type == "document" then
             assert(level == 0, "document nodes cannot be nested")
             buf:write("{\n")
             local i1 = indent[level+1]
-            buf:write(fmt(sfmt, i1, "type", "document"))
-            buf:write(fmt(bfmt, i1, "has_doctype", node.has_doctype))
-            buf:write(fmt(sfmt, i1, "name", node.name))
-            buf:write(fmt(sfmt, i1, "system_identifier", node.system_identifier))
-            buf:write(fmt(sfmt, i1, "public_identifier", node.public_identifier))
-            buf:write(fmt(sfmt, i1, "quirks_mode", node.quirks_mode))
+            buf:write(i1, 'type = "document",\n')
+            buf:write(i1, 'has_doctype = ', tostring(node.has_doctype), ',\n')
+            buf:write(i1, 'name = "', node.name, '",\n')
+            buf:write(i1, 'system_identifier = "', node.system_identifier, '",\n')
+            buf:write(i1, 'public_identifier = "', node.public_identifier, '",\n')
+            buf:write(i1, 'quirks_mode = "', node.quirks_mode, '",\n')
             for i = 1, #node do
                 serialize(node[i], level + 1)
             end
