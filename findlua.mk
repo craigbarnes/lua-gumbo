c74c3055f56664ab139a48da59143129f4954791
Craig Barnes <Cr@igBarn.es>
2014-02-08 20:35:12 +0000

    Clean up findlua.mk

--- findlua.mk
+++ findlua.mk
@@ -7,24 +7,23 @@ PKGCONFIG    ?= pkg-config --silence-errors
 # - FreeBSD and some others seem to be considering lua-5.2.pc and lua-5.1.pc
 LUA_PC_NAMES  = lua lua52 lua5.2 lua-5.2 lua51 lua5.1 lua-5.1 luajit
 
-LUA_PC_FOUND  = $(strip $(foreach file, $(LUA_PC_NAMES), \
-                $(if $(shell $(PKGCONFIG) --libs $(file)),$(file),)))
+LUA_PC_FOUND  = $(strip $(foreach PC, $(LUA_PC_NAMES), \
+                $(if $(shell $(PKGCONFIG) --exists $(PC) && echo 1),$(PC),)))
 
 LUA_PC_FIRST  = $(firstword $(LUA_PC_FOUND))
 
 LUA_PC        = $(if $(LUA_PC_FIRST),$(LUA_PC_FIRST), \
                 $(error No pkg-config file found for Lua))
 
-# Some distros put the Lua headers in versioned sub-directories, which
-# aren't in the default include paths and hence require extra CFLAGS
+# Some distros put the Lua headers in versioned sub-directories
+# and thus require extra CFLAGS
 LUA_CFLAGS    = $(shell $(PKGCONFIG) --cflags $(LUA_PC))
 
 # Some pkg-config files have convenient variables for module paths
-# (i.e. were written by people who actually use Lua)
 LUA_PC_LMOD   = $(shell $(PKGCONFIG) --variable=INSTALL_LMOD $(LUA_PC))
 LUA_PC_CMOD   = $(shell $(PKGCONFIG) --variable=INSTALL_CMOD $(LUA_PC))
 
-# Others were written by idiots, who force us to do this...
+# Others force us to piece them together from parts...
 LUA_PREFIX    = $(shell $(PKGCONFIG) --variable=prefix $(LUA_PC))
 LUA_LIBDIR    = $(shell $(PKGCONFIG) --variable=libdir $(LUA_PC))
 LUA_VERSION   = $(shell $(PKGCONFIG) --modversion $(LUA_PC) | grep -o '^.\..')
