6693491cb7c931cde984894d20c4c735ca0ec1b6
Craig Barnes <Cr@igBarn.es>
2014-03-06 09:15:36 +0000

    Simplify test/serialize.lua

--- test/serialize.lua
+++ test/serialize.lua
@@ -3,42 +3,24 @@
 -- Can be used with the diff utility for testing against expected output.
 
 local gumbo = require "gumbo"
-local commands = {}
-setmetatable(commands, commands)
+local fmt = string.format
 
-local usage = [[
-Usage: %s COMMAND [INPUT-FILE] [OUTPUT-FILE]
+local commands = {
+    html     = "Parse and serialize back to HTML",
+    table    = "Parse and serialize to Lua table",
+    html5lib = "Parse and serialize to html5lib tree-constructor format",
+}
 
-Commands:
-
-   html            Parse and serialize back to HTML
-   table           Parse and serialize to Lua table
-   html5lib        Parse and serialize to html5lib tree-constructor format
-   parse           Parse and exit (for benchmarking)
-   help            Print usage information and exit
-
-]]
-
-function commands.help()
-    io.stdout:write(string.format(usage, arg[0]))
+local function usage(fd, progname)
+    fd:write("Usage: ", progname, " COMMAND [INPUT-FILE] [OUTPUT-FILE]")
+    fd:write("\n\nCommands:\n\n")
+    for k, v in pairs(commands) do fd:write(fmt("   %-10s  %s\n", k, v)) end
+    fd:write("\n")
+    os.exit(1)
 end
 
-function commands.parse(input)
-    return assert(gumbo.parse_file(input))
-end
-
-function commands.__index(self, key)
-    if type(key) == "string" then
-        local exists, serialize = pcall(require, "gumbo.serialize." .. key)
-        if exists then
-            return function(input, output)
-                assert(serialize(self.parse(input), output))
-            end
-        end
-    end
-    return self.help
-end
-
-local input = arg[2] or io.stdin
+local command = commands[arg[1]] and arg[1] or usage(io.stderr, arg[0])
+local serialize = require("gumbo.serialize." .. command)
 local output = arg[3] and assert(io.open(arg[3], "a")) or io.stdout
-commands[arg[1]](input, output)
+local tree = assert(gumbo.parse_file(arg[2] or io.stdin))
+assert(serialize(tree, output))
