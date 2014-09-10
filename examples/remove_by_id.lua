eefcb4590706ebbe5615f22596a8e65ed98ca800
Craig Barnes <Cr@igBarn.es>
2014-09-10 05:12:48 +0100

    Tweak examples/remove_by_id.lua

--- examples/remove_by_id.lua
+++ examples/remove_by_id.lua
@@ -1,16 +1,14 @@
 #!/usr/bin/env lua
 local gumbo = require "gumbo"
 local to_html = require "gumbo.serialize.html"
+local id, filename = arg[1], arg[2]
 
-if not arg[1] then
-    io.stderr:write(
-        "Error: No element ID specified\n",
-        "Usage: ", arg[0], " ELEMENT-ID [FILENAME]\n"
-    )
+if not id or id == "-h" or id == "--help" then
+    io.stderr:write("Usage: ", arg[0], " ELEMENT-ID [FILENAME]\n")
     os.exit(1)
 end
 
-local document = assert(gumbo.parse_file(arg[2] or io.stdin))
-local element = document:getElementById(arg[1])
+local document = assert(gumbo.parse_file(filename or io.stdin))
+local element = document:getElementById(id)
 if element then element:remove() end
 to_html(document, io.stdout)
