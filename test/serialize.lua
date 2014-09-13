27a085ce5991f260677b0ba3236ca3f759b5abe5
Craig Barnes <Cr@igBarn.es>
2014-09-13 01:13:14 +0100

    Clean up serialize.lua

--- test/serialize.lua
+++ test/serialize.lua
@@ -3,23 +3,26 @@
 -- Can be used with the diff utility for testing against expected output.
 
 local gumbo = require "gumbo"
-local fmt = string.format
+local stderr = io.stderr
+local progname = arg[0]
 
 local commands = {
     html     = "Parse and serialize back to HTML",
     table    = "Parse and serialize to Lua table",
-    html5lib = "Parse and serialize to html5lib tree-constructor format",
+    html5lib = "Parse and serialize to html5lib AST",
 }
 
-local function usage(fd, progname)
-    fd:write("Usage: ", progname, " COMMAND [INPUT-FILE] [OUTPUT-FILE]")
-    fd:write("\n\nCommands:\n\n")
-    for k, v in pairs(commands) do fd:write(fmt("   %-10s  %s\n", k, v)) end
-    fd:write("\n")
+local function usage()
+    stderr:write("Usage: ", progname, " COMMAND [INPUT-FILE] [OUTPUT-FILE]")
+    stderr:write("\n\nCommands:\n\n")
+    for command, description in pairs(commands) do
+        stderr:write(string.format("   %-10s  %s\n", command, description))
+    end
+    stderr:write("\n")
     os.exit(1)
 end
 
-local command = commands[arg[1]] and arg[1] or usage(io.stderr, arg[0])
+local command = commands[arg[1]] and arg[1] or usage()
 local serialize = require("gumbo.serialize." .. command)
 local output = arg[3] and assert(io.open(arg[3], "a")) or io.stdout
 local tree = assert(gumbo.parse_file(arg[2] or io.stdin))
