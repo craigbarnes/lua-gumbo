69295047341aa10e993497df3b7f699409f57a8a
Craig Barnes <Cr@igBarn.es>
2014-10-01 02:44:40 +0100

    Re-implement examples/find_links.lua using Node:walk() iterator

--- examples/find_links.lua
+++ examples/find_links.lua
@@ -1,19 +1,12 @@
 #!/usr/bin/env lua
 local gumbo = require "gumbo"
+local document = assert(gumbo.parse_file(arg[1] or io.stdin))
 
---- Iterate an element node recursively, printing any href attributes found
-local function find_links(node)
-    if node.type == "element" then
+for node in document:walk() do
+    if node.type == "element" and node.localName == "a" then
         local href = node.attributes.href
         if href then
             print(href.value)
         end
-        local children = node.childNodes
-        for i = 1, #children do
-            find_links(children[i])
-        end
     end
 end
-
-local document = assert(gumbo.parse_file(arg[1] or io.stdin))
-find_links(document.documentElement)
