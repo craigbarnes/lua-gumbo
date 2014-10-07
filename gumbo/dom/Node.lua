565ae7d5211d290a628c4159681ff83005150c87
Craig Barnes <Cr@igBarn.es>
2014-10-07 22:53:19 +0100

    Add DOCUMENT_POSITION_* constants to Node.lua

--- gumbo/dom/Node.lua
+++ gumbo/dom/Node.lua
@@ -17,6 +17,14 @@ local Node = {
     DOCUMENT_FRAGMENT_NODE = 11,
     NOTATION_NODE = 12, -- historical
 
+    DOCUMENT_POSITION_DISCONNECTED = 0x01,
+    DOCUMENT_POSITION_PRECEDING = 0x02,
+    DOCUMENT_POSITION_FOLLOWING = 0x04,
+    DOCUMENT_POSITION_CONTAINS = 0x08,
+    DOCUMENT_POSITION_CONTAINED_BY = 0x10,
+    DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC = 0x20,
+    -- TODO: function Node:compareDocumentPosition(other)
+
     childNodes = {length = 0},
     getters = getters
 }
