49957328003dd2009162dfc1e6d8c4c0a9cfe678
Craig Barnes <Cr@igBarn.es>
2014-11-17 02:34:48 +0000

    Remove GumboParseFlags constants from gumbo module table

--- gumbo.lua
+++ gumbo.lua
@@ -38,15 +38,4 @@ end
 return {
     parse = parse,
     parse_file = parse_file,
-    -- GumboParseFlags:
-    insertion_by_parser = 2^0,
-    implicit_end_tag = 2^1,
-    insertion_implied = 2^3,
-    converted_from_end_tag = 2^4,
-    insertion_from_isindex = 2^5,
-    insertion_from_image = 2^6,
-    reconstructed_formatting_element = 2^7,
-    adoption_agency_cloned = 2^8,
-    adoption_agency_moved = 2^9,
-    foster_parented = 2^10
 }
