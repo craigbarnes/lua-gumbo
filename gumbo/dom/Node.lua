136ce7d6b9babc94039ca0748e9cc544c18d5cf7
Craig Barnes <Cr@igBarn.es>
2014-11-19 00:58:20 +0000

    Minor reformatting

--- gumbo/dom/Node.lua
+++ gumbo/dom/Node.lua
@@ -2,23 +2,25 @@ local NodeList = require "gumbo.dom.NodeList"
 local Set = require "gumbo.Set"
 local yield, wrap = coroutine.yield, coroutine.wrap
 local tinsert, tremove = table.insert, table.remove
-local ipairs, assert, setmetatable = ipairs, assert, setmetatable
-local type = type
+local ipairs, type, assert, setmetatable = ipairs, type, assert, setmetatable
 local _ENV = nil
 
 local Node = {
+    -- Valid node types
     ELEMENT_NODE = 1,
-    ATTRIBUTE_NODE = 2, -- historical
     TEXT_NODE = 3,
-    CDATA_SECTION_NODE = 4, -- historical
-    ENTITY_REFERENCE_NODE = 5, -- historical
-    ENTITY_NODE = 6, -- historical
     PROCESSING_INSTRUCTION_NODE = 7,
     COMMENT_NODE = 8,
     DOCUMENT_NODE = 9,
     DOCUMENT_TYPE_NODE = 10,
     DOCUMENT_FRAGMENT_NODE = 11,
-    NOTATION_NODE = 12, -- historical
+
+    -- Obsolete node types
+    ATTRIBUTE_NODE = 2,
+    CDATA_SECTION_NODE = 4,
+    ENTITY_REFERENCE_NODE = 5,
+    ENTITY_NODE = 6,
+    NOTATION_NODE = 12,
 
     DOCUMENT_POSITION_DISCONNECTED = 0x01,
     DOCUMENT_POSITION_PRECEDING = 0x02,
