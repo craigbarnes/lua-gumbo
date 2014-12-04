fea407288755e3268d828fadc4de50dc6a2f32c9
Craig Barnes <Cr@igBarn.es>
2014-12-04 12:43:01 +0000

    Add test for "luarocks make" to .travis.mk

--- .travis.mk
+++ .travis.mk
@@ -2,9 +2,12 @@ ifeq "$(TRAVIS_OS_NAME)" "linux"
   PM_INSTALL = sudo apt-get -y install
   PM_UPDATE_CACHE = sudo apt-get update -qq
   LD_UPDATE_CACHE = sudo ldconfig
-  PACKAGES_lua5.1 = lua5.1 liblua5.1-dev
+  PACKAGES_lua5.1 = lua5.1 liblua5.1-dev luarocks
   PACKAGES_lua5.2 = lua5.2 liblua5.2-dev
   PACKAGES_luajit = luajit libluajit-5.1-dev
+  ifeq "$(LUA_PC)" "lua5.1"
+    EXTRA_TESTS = make clean && sudo luarocks make gumbo-scm-1.rockspec
+  endif
   ifeq "$(LUA_PC)" "luajit"
     PM_ADD_REPO = sudo add-apt-repository -y ppa:mwild1/ppa
   endif
@@ -14,8 +17,11 @@ ifeq "$(TRAVIS_OS_NAME)" "osx"
   PM_INSTALL = brew install
   PM_UPDATE_CACHE = brew update
   PACKAGES_lua5.1 = lua51
-  PACKAGES_lua5.2 = lua
+  PACKAGES_lua5.2 = lua luarocks
   PACKAGES_luajit = luajit
+  ifeq "$(LUA_PC)" "lua5.2"
+    EXTRA_TESTS = make clean && luarocks make gumbo-scm-1.rockspec
+  endif
 endif
 
 ifeq "$(LUA_PC)" "luajit"
