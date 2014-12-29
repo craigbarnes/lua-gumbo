7591f7d07b0e3e2dcd634406dd36c0411334f50d
Craig Barnes <Cr@igBarn.es>
2014-12-29 03:13:59 +0000

    Add Set:union() and Set:__add() methods

--- gumbo/Set.lua
+++ gumbo/Set.lua
@@ -1,21 +1,60 @@
-local type, ipairs, error = type, ipairs, error
+local type, pairs, error = type, pairs, error
+local getmetatable, setmetatable = getmetatable, setmetatable
 local _ENV = nil
+local Set = {}
+Set.__index = Set
 
-local function Set(members)
-    local type = type(members)
-    local set = {}
+local function addKeys(set, t)
+    for member in pairs(t) do
+        set[member] = true
+    end
+end
+
+local function addValues(set, t)
+    for i = 1, #t do
+        set[t[i]] = true
+    end
+end
+
+local function addWords(set, s)
+    for member in s:gmatch("%S+") do
+        set[member] = true
+    end
+end
+
+function Set:union(other)
+    local union = {}
+    local type = type(other)
     if type == "table" then
-        for i, member in ipairs(members) do
-            set[member] = true
+        if getmetatable(other) == Set then
+            addKeys(union, other)
+        else
+            addValues(union, other)
         end
     elseif type == "string" then
-        for member in members:gmatch("%S+") do
-            set[member] = true
-        end
+        addWords(union, other)
+    else
+        error("Invalid argument type; expecting Set, table or string", 2)
+    end
+    for member in pairs(self) do
+        union[member] = true
+    end
+    return setmetatable(union, Set)
+end
+
+Set.__add = Set.union
+
+local function constructor(members)
+    local set = {}
+    local type = type(members)
+    if type == "table" then
+        addValues(set, members)
+    elseif type == "string" then
+        addWords(set, members)
     else
         error("Invalid argument type; expecting table or string", 2)
     end
-    return set
+    return setmetatable(set, Set)
 end
 
-return Set
+return constructor
