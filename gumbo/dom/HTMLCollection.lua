9524973c0d6a0f8644782c7f8db85d9f5d1f7785
Craig Barnes <Cr@igBarn.es>
2015-11-26 19:51:33 +0000

    Fix HTMLCollection.namedItem to properly follow the DOM spec

--- gumbo/dom/HTMLCollection.lua
+++ gumbo/dom/HTMLCollection.lua
@@ -17,13 +17,19 @@ function HTMLCollection:item(index)
     return self[index]
 end
 
-function HTMLCollection:namedItem(name)
-    assertString(name)
-    if name ~= "" then
-        for i, element in ipairs(self) do
-            if element.id == name or element:getAttribute("name") == name then
-                return element
-            end
+function HTMLCollection:namedItem(key)
+    assertString(key)
+    if key == "" then
+        return nil
+    end
+    for _, element in ipairs(self) do
+        if element:getAttribute("id") == key then
+            return element
+        elseif
+            element.namespaceURI == "http://www.w3.org/1999/xhtml"
+            and element:getAttribute("name") == key
+        then
+            return element
         end
     end
 end
