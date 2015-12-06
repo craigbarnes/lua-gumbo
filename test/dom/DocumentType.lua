5d0feac9338472f4789869a7681ea09507067544
Craig Barnes <Cr@igBarn.es>
2015-12-06 13:57:50 +0000

    Add test coverage for DocumentType:cloneNode()

--- test/dom/DocumentType.lua
+++ test/dom/DocumentType.lua
@@ -1,19 +1,41 @@
 local gumbo = require "gumbo"
-local assert = assert
+local assert, rawequal, getmetatable = assert, rawequal, getmetatable
 local _ENV = nil
 
-local input = "<!doctype html><p>no-quirks!</p>"
-local document = assert(gumbo.parse(input))
-local doctype = assert(document.doctype)
+do
+    local input = "<!doctype html><p>no-quirks!</p>"
+    local document = assert(gumbo.parse(input))
+    local doctype = assert(document.doctype)
 
-assert(document.compatMode == "CSS1Compat")
-assert(doctype.nodeType == document.DOCUMENT_TYPE_NODE)
-assert(doctype.nodeName == doctype.name)
-assert(doctype.name == "html")
-assert(doctype.publicId == "")
-assert(doctype.systemId == "")
+    assert(document.compatMode == "CSS1Compat")
+    assert(doctype.nodeType == document.DOCUMENT_TYPE_NODE)
+    assert(doctype.nodeName == doctype.name)
+    assert(doctype.name == "html")
+    assert(doctype.publicId == "")
+    assert(doctype.systemId == "")
 
-doctype.publicId = nil
-assert(doctype.publicId == "")
-doctype.systemId = nil
-assert(doctype.systemId == "")
+    doctype.publicId = nil
+    assert(doctype.publicId == "")
+    doctype.systemId = nil
+    assert(doctype.systemId == "")
+end
+
+do
+    local pubid = "-//W3C//DTD XHTML 1.1//EN"
+    local sysid = "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"
+    local input = ('<!DOCTYPE html PUBLIC "%s" "%s">'):format(pubid, sysid)
+    local document = assert(gumbo.parse(input))
+    local doctype = assert(document.doctype)
+    assert(rawequal(doctype, document.childNodes[1]))
+    assert(doctype.name == "html")
+    assert(doctype.publicId == pubid)
+    assert(doctype.systemId == sysid)
+    local clone = assert(doctype:cloneNode())
+    assert(not rawequal(clone, doctype))
+    assert(not rawequal(clone, document.childNodes[1]))
+    assert(clone.name == "html")
+    assert(clone.publicId == pubid)
+    assert(clone.systemId == sysid)
+    assert(getmetatable(clone))
+    assert(getmetatable(clone) == getmetatable(doctype))
+end
