864d46e5b3160b5decf1ca7bd1fc80d00c8fc71b
Craig Barnes <Cr@igBarn.es>
2015-12-23 00:18:13 +0000

    Configure GitLab pages

--- mk/doc.mk
+++ mk/doc.mk
@@ -1,26 +1,24 @@
 PANDOC = pandoc
-DATE = $(shell date +'%B %d, %Y')
-DOC_TARGETS = README.html README.pdf doc/api.html
 
-docs: $(DOC_TARGETS)
+docs: public/index.html public/api.html
 
-doc/%.html: doc/%.md doc/template.html doc/style.css.inc
+public/%.html: doc/%.md doc/template.html doc/style.css.inc | public/
 	$(PANDOC) -S --toc --template $(word 2, $^) -H $(word 3, $^) -o $@ $<
 
-README.html: README.md doc/template.html doc/style.css.inc
+public/index.html: README.md doc/template.html doc/style.css.inc | public/
 	$(PANDOC) -S --toc --template $(word 2, $^) -H $(word 3, $^) -o $@ $<
-
-README.pdf: doc/metadata.yml README.md
-	sed '/^\[!\[/d' $^ | \
-	  $(PANDOC) --toc -M date='$(DATE)' -V geometry:margin=3.5cm -o $@
 
 doc/style.css.inc: doc/style.css
 	echo '<style>' > $@
 	cat $< >> $@
 	echo '</style>' >> $@
 
+public/:
+	$(MKDIR) $@
+
 clean-docs:
-	$(RM) $(DOC_TARGETS) doc/style.css.inc
+	$(RM) doc/style.css.inc
+	$(RM) -r public/
 
 
 .PHONY: docs clean-docs
