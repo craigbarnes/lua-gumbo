c9249f2cfa3100993161394e15a358ddca179c03
Craig Barnes <Cr@igBarn.es>
2016-07-08 12:37:04 +0100

    Tweak output format of test/checkurl.sh script

--- test/checkurl.sh
+++ test/checkurl.sh
@@ -2,9 +2,5 @@
 set -e
 
 while read URL; do
-    tput bold
-    echo "$URL"
-    tput sgr0
-    curl -sS --head -w '%{http_code}  %{redirect_url}\n' -o /dev/null "$URL"
-    echo
+    curl -sS --head -w " %{http_code}  $URL  %{redirect_url}\n" -o /dev/null "$URL"
 done
