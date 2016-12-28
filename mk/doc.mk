81136996398322728fe0e78d55b8a33ada4ff8fe
Craig Barnes <Cr@igBarn.es>
2016-12-28 15:33:31 +0000

    Append code examples to HTML documentation page

--- mk/doc.mk
+++ mk/doc.mk
@@ -1,11 +1,14 @@
 CHECKURL = curl -sSI -w "%{http_code}  $$URL  %{redirect_url}\n" -o /dev/null
 PANDOC = pandoc
+EXAMPLE_NAMES = find_links get_title remove_by_id table_align_fix text_content
+EXAMPLE_FILES = $(addprefix examples/, $(addsuffix .lua, $(EXAMPLE_NAMES)))
+DOC_FILES = README.md doc/api.md build/examples.md
 
 docs: public/index.html public/api.html
 
-public/index.html: README.md doc/api.md doc/template.html doc/style.css.inc | public/
+public/index.html: $(DOC_FILES) doc/template.html doc/style.css.inc | public/
 	$(PANDOC) --smart --toc --include-in-header=doc/style.css.inc \
-	  --template=doc/template.html --output=$@ README.md doc/api.md
+	  --template=doc/template.html --output=$@ $(DOC_FILES)
 
 public/api.html: doc/redir.html | public/
 	cp $< $@
@@ -14,6 +17,14 @@ doc/style.css.inc: doc/style.css
 	echo '<style>' > $@
 	cat $< >> $@
 	echo '</style>' >> $@
+
+build/examples.md: $(EXAMPLE_FILES) | build/
+	printf "## Examples\n\n" > $@
+	for file in $^; do \
+	  printf '```lua\n' >> $@; \
+	  cat $$file >> $@; \
+	  printf '```\n\n' >> $@; \
+	done
 
 public/:
 	$(MKDIR) $@
