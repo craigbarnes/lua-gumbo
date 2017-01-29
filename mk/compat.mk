374c1973b42458c19ae305ac896f14844b6f3aa1
Craig Barnes <Cr@igBarn.es>
2017-01-29 23:29:46 +0000

    Use $(filter) instead of $(findstring) to query GNU Make $(.FEATURES)
    The findstring macro looks for a substring anywhere in a string, whereas
    filter looks for whole, whitespace-separated words.
    
    Calling $(findstring, else-i, else-if) returns a positive match, which
    is definitely not the desired behaviour for checking the $(.FEATURES)
    list.

--- mk/compat.mk
+++ mk/compat.mk
@@ -1,4 +1,4 @@
-HASFEATURE = $(findstring $(1), $(.FEATURES))
+HASFEATURE = $(filter $(1), $(.FEATURES))
 REQERROR   = Required feature "$(strip $(1))" not supported by $(MAKE)
 REQUIRE    = $(and $(or $(HASFEATURE), $(error $(REQERROR))),)
 
