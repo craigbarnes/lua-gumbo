5c46e672254c1cf1493971595a9ef018bac21618
Craig Barnes <Cr@igBarn.es>
2017-03-26 10:06:33 +0100

    Remove :item() method and TODO list from AttributeList.lua
    Attributes should be accessed via their parent element. There's no
    reason to implement all the pointless NamedNodeMap methods from the DOM
    spec.
    
    This isn't a breaking change, since the :item() method was never
    actually made available via any metatable.

--- gumbo/dom/AttributeList.lua
+++ gumbo/dom/AttributeList.lua
@@ -7,16 +7,4 @@ function AttributeList:__index(k)
     end
 end
 
--- TODO: Add tests
-function AttributeList:item(index)
-    return self[index]
-end
-
--- TODO: function AttributeList:getNamedItem(name)
--- TODO: function AttributeList:getNamedItemNS(namespace, localName)
--- TODO: function AttributeList:setNamedItem(attr)
--- TODO: function AttributeList:setNamedItemNS(attr)
--- TODO: function AttributeList:removeNamedItem(name)
--- TODO: function AttributeList:removeNamedItemNS(namespace, localName)
-
 return AttributeList
