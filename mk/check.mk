1bf4a1ab93a62f2c0078a0470c11bfc438166c42
Craig Barnes <Cr@igBarn.es>
2017-07-26 03:34:51 +0100

    Try to find LuaJIT interpreter automatically for "make check-luajit"
    The name of the LuaJIT command on OpenBSD 6 is "luajit51". The 5.1 ABI
    is the one we actually want, so try that name first.

--- mk/check.mk
+++ mk/check.mk
@@ -1,3 +1,8 @@
+LUAJIT ?= $(or \
+    $(shell command -v luajit51 || command -v luajit), \
+    $(error Unable to find luajit command) \
+)
+
 CHECK_ALL = $(addprefix check-, $(BUILD_VERS))
 
 check-all: $(CHECK_ALL) check-luajit
@@ -6,7 +11,7 @@ $(CHECK_ALL): check-lua%: build-lua%
 	LUA_CPATH=build/lua$*/?.so $(LUA$*) runtests.lua
 
 check-luajit: build-lua51
-	LUA_CPATH=build/lua51/?.so $(or $(LUAJIT),luajit) runtests.lua
+	LUA_CPATH=build/lua51/?.so $(LUAJIT) runtests.lua
 
 coverage.txt: build-lua53
 	LUA_CPATH=build/lua53/?.so $(LUA53) -lluacov runtests.lua
