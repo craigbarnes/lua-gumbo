f2b64fa882580e491025f880f11cf6c1038954ca
Craig Barnes <Cr@igBarn.es>
2017-08-16 06:05:39 +0100

    Clean up table_align_fix example and rewrite to use Node:walk()

--- examples/table_align_fix.lua
+++ examples/table_align_fix.lua
@@ -1,28 +1,29 @@
--- Replaces all "align" attributes with the CSS "text-align" property.
--- This can be used to filter Pandoc HTML output, which uses the
--- (deprecated) "align" attribute on th and td elements.
+-- Replaces all "align" attributes on td/th elements with a "style"
+-- attribute containing the equivalent CSS "text-align" property.
+-- This can be used to fix Pandoc HTML output.
 
 local gumbo = require "gumbo"
+local Set = require "gumbo.Set"
 local document = assert(gumbo.parseFile(arg[1] or io.stdin))
+local alignments = Set{"left", "right", "center", "start", "end"}
 
-local function fixAlignAttr(elements)
-    for i, element in ipairs(elements) do
-        local align = element:getAttribute("align")
-        if align then
-            if align ~= "left" then -- left is the default for ltr languages
-                local css = "text-align:" .. align
-                local style = element:getAttribute("style")
-                if style then
-                    css = style .. "; " .. css
-                end
-                element:setAttribute("style", css)
-            end
-            element:removeAttribute("align")
+local function fixAlignAttr(element)
+    local align = element:getAttribute("align")
+    if align and alignments[align] then
+        local style = element:getAttribute("style")
+        if style then
+            element:setAttribute("style", style .. "; text-align:" .. align)
+        else
+            element:setAttribute("style", "text-align:" .. align)
         end
+        element:removeAttribute("align")
     end
 end
 
-fixAlignAttr(document:getElementsByTagName("td"))
-fixAlignAttr(document:getElementsByTagName("th"))
+for node in document.body:walk() do
+    if node.localName == "td" or node.localName == "th" then
+        fixAlignAttr(node)
+    end
+end
 
 document:serialize(io.stdout)
