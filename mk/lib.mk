56d83fed099ace7272db75084a85aff94c16c143
Craig Barnes <Cr@igBarn.es>
2017-09-29 21:00:06 +0100

    Use gperf-generated perfect hash to implement gumbo_tagn_enum
    This time with a sane build system.

--- mk/lib.mk
+++ mk/lib.mk
@@ -1,6 +1,6 @@
 LIBGUMBO_FILES = \
-    attribute error string_buffer tag utf8 vector char_ref parser \
-    string_piece tokenizer util \
+    attribute error string_buffer tag tag_lookup utf8 vector char_ref \
+    parser string_piece tokenizer util
 
 LIBGUMBO_OBJ = $(addprefix build/lib/, $(addsuffix .o, $(LIBGUMBO_FILES)))
 
@@ -13,8 +13,15 @@ $(LIBGUMBO_OBJ): build/lib/%.o: lib/%.c | build/lib/
 build/lib/:
 	@$(MKDIR) '$@'
 
+ragel-gen:
+	ragel -F0 -o lib/char_ref.c lib/char_ref.rl
+
+gperf-gen:
+	gperf -LANSI-C -m200 lib/tag_lookup.gperf > lib/tag_lookup.c
+
 
 CLEANFILES += $(LIBGUMBO_OBJ)
+.PHONY: ragel-gen gperf-gen
 
 # sed -i '/^# sed/,$ { /^# sed/b; /^  gcc/b; d }' mk/lib.mk && \
   gcc -MM lib/*.c | sed 's|^\([^: ]\+:\)|build/lib/\1|' >> mk/lib.mk
@@ -33,7 +40,8 @@ build/lib/string_buffer.o: lib/string_buffer.c lib/string_buffer.h lib/gumbo.h \
  lib/string_piece.h lib/util.h
 build/lib/string_piece.o: lib/string_piece.c lib/string_piece.h lib/gumbo.h \
  lib/util.h
-build/lib/tag.o: lib/tag.c lib/gumbo.h lib/tag_strings.h
+build/lib/tag.o: lib/tag.c lib/gumbo.h lib/tag_lookup.h lib/tag_strings.h
+build/lib/tag_lookup.o: lib/tag_lookup.c lib/tag_lookup.h lib/gumbo.h
 build/lib/tokenizer.o: lib/tokenizer.c lib/tokenizer.h lib/gumbo.h lib/token_type.h \
  lib/tokenizer_states.h lib/attribute.h lib/char_ref.h lib/error.h \
  lib/insertion_mode.h lib/string_buffer.h lib/parser.h lib/string_piece.h \
