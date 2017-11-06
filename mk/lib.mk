0ada74f33e28ada7172bc7a49a4a6917d1cb7545
Craig Barnes <Cr@igBarn.es>
2017-11-06 16:56:25 +0000

    Regenerate lib/tag_lookup.c

--- mk/lib.mk
+++ mk/lib.mk
@@ -56,11 +56,12 @@ build/lib/error.o: lib/error.c lib/error.h lib/gumbo.h lib/insertion_mode.h \
 build/lib/parser.o: lib/parser.c lib/attribute.h lib/gumbo.h lib/error.h \
  lib/insertion_mode.h lib/string_buffer.h lib/token_type.h lib/parser.h \
  lib/tokenizer.h lib/tokenizer_states.h lib/utf8.h lib/util.h \
- lib/vector.h
+ lib/vector.h lib/replacement.h
 build/lib/string_buffer.o: lib/string_buffer.c lib/string_buffer.h lib/gumbo.h \
  lib/string_piece.h lib/util.h
 build/lib/string_piece.o: lib/string_piece.c lib/string_piece.h lib/gumbo.h \
  lib/util.h
+build/lib/svg_attrs.o: lib/svg_attrs.c lib/replacement.h
 build/lib/tag.o: lib/tag.c lib/gumbo.h lib/tag_lookup.h
 build/lib/tag_lookup.o: lib/tag_lookup.c lib/tag_lookup.h lib/gumbo.h
 build/lib/tokenizer.o: lib/tokenizer.c lib/tokenizer.h lib/gumbo.h lib/token_type.h \
