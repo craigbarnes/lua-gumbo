8aaae2b69514b2faf46ce31847faa650dd829eca
Craig Barnes <Cr@igBarn.es>
2018-01-31 05:07:00 +0000

    Clean up some verbosity in mk/lua.mk

--- mk/compat.mk
+++ mk/compat.mk
@@ -5,3 +5,9 @@ REQUIRE    = $(and $(or $(HASFEATURE), $(error $(REQERROR))),)
 $(call REQUIRE, else-if)
 $(call REQUIRE, order-only)
 $(call REQUIRE, target-specific)
+
+ifeq "$(MAKE_VERSION)" "3.81"
+  $(warning Disabling build optimization to work around a bug in GNU Make 3.81)
+else
+  make-lazy = $(eval $1 = $$(eval $1 := $(value $(1)))$$($1))
+endif
--- mk/lua.mk
+++ mk/lua.mk
@@ -1,8 +1,9 @@
 PKGCONFIG = pkg-config
-PKGCONFIG_Q = $(PKGCONFIG) --silence-errors 2>/dev/null
-PKGEXISTS = $(PKGCONFIG_Q) --exists $(1) && echo $(1)
+PKGEXISTS = $(PKGCONFIG) --exists $(1) && echo $(1)
 PKGFIND = $(shell for P in $(1); do $(call PKGEXISTS, $$P) && break; done)
-PKGMATCH = $(shell $(PKGCONFIG_Q) --exists '$(1) $(2); $(1) $(3)' && echo $(1))
+PKGMATCH = $(shell $(PKGCONFIG) --exists '$(1) $(2); $(1) $(3)' && echo $(1))
+LFLAG = $(shell $(PKGCONFIG) $(1) $(LUA$(strip $(2))_PC))
+LVAR = $(call LFLAG, --variable=$(strip $(1)), $(2))
 CMDFIND = $(shell for C in $(1); do command -v $$C && break; done)
 
 LUA53_PC ?= $(or \
@@ -21,34 +22,24 @@ LUA51_PC ?= $(or \
     $(call PKGMATCH, luajit, >= 2.0.0, < 2.2.0) \
 )
 
-ifneq "$(MAKE_VERSION)" "3.81"
-  # Use "initially recursive" trick so that these variables are only
-  # expanded (at most) once per build.
-  make-lazy = $(eval $1 = $$(eval $1 := $(value $(1)))$$($1))
-  $(call make-lazy,LUA53_PC)
-  $(call make-lazy,LUA52_PC)
-  $(call make-lazy,LUA51_PC)
-else
-  $(warning Disabling optimizations to work around a bug in GNU Make 3.81)
-  $(warning Expect much slower build speeds)
-endif
+$(foreach VER, 53 52 51, $(call make-lazy,LUA$(VER)_PC))
 
 LUAS_FOUND = \
     $(if $(LUA53_PC), lua53) \
     $(if $(LUA52_PC), lua52) \
     $(if $(LUA51_PC), lua51)
 
-LUA53_CFLAGS ?= $(shell $(PKGCONFIG_Q) --cflags $(LUA53_PC))
-LUA52_CFLAGS ?= $(shell $(PKGCONFIG_Q) --cflags $(LUA52_PC))
-LUA51_CFLAGS ?= $(shell $(PKGCONFIG_Q) --cflags $(LUA51_PC))
+LUA53_CFLAGS ?= $(call LFLAG, --cflags, 53)
+LUA52_CFLAGS ?= $(call LFLAG, --cflags, 52)
+LUA51_CFLAGS ?= $(call LFLAG, --cflags, 51)
 
-LUA53_LMODDIR ?= $(shell $(PKGCONFIG_Q) --variable=INSTALL_LMOD $(LUA53_PC))
-LUA52_LMODDIR ?= $(shell $(PKGCONFIG_Q) --variable=INSTALL_LMOD $(LUA52_PC))
-LUA51_LMODDIR ?= $(shell $(PKGCONFIG_Q) --variable=INSTALL_LMOD $(LUA51_PC))
+LUA53_LMODDIR ?= $(call LVAR, INSTALL_LMOD, 53)
+LUA52_LMODDIR ?= $(call LVAR, INSTALL_LMOD, 52)
+LUA51_LMODDIR ?= $(call LVAR, INSTALL_LMOD, 51)
 
-LUA53_CMODDIR ?= $(shell $(PKGCONFIG_Q) --variable=INSTALL_CMOD $(LUA53_PC))
-LUA52_CMODDIR ?= $(shell $(PKGCONFIG_Q) --variable=INSTALL_CMOD $(LUA52_PC))
-LUA51_CMODDIR ?= $(shell $(PKGCONFIG_Q) --variable=INSTALL_CMOD $(LUA51_PC))
+LUA53_CMODDIR ?= $(call LVAR, INSTALL_CMOD, 53)
+LUA52_CMODDIR ?= $(call LVAR, INSTALL_CMOD, 52)
+LUA51_CMODDIR ?= $(call LVAR, INSTALL_CMOD, 51)
 
 LUA53 ?= $(call CMDFIND, $(LUA53_PC) lua5.3 lua-5.3 lua53)
 LUA52 ?= $(call CMDFIND, $(LUA52_PC) lua5.2 lua-5.2 lua52)
