8b47be8037c9aab1868e1c7e3d6800b82d61a9fa
Craig Barnes <Cr@igBarn.es>
2018-02-02 08:49:14 +0000

    Minor optimizations in gumbo/serialize/Indent.lua

--- gumbo/serialize/Indent.lua
+++ gumbo/serialize/Indent.lua
@@ -1,17 +1,18 @@
-local rep, setmetatable = string.rep, setmetatable
+local setmetatable = setmetatable
 local _ENV = nil
 local Indent = {}
 
 function Indent:__index(depth)
     if depth < 25 then
-        self[depth] = self[1]:rep(depth)
-        return self[depth]
+        local indent = self[1]:rep(depth)
+        self[depth] = indent
+        return indent
     else -- stop indenting at depth of 25, to avoid buffer/output bloat
         return ""
     end
 end
 
 return function(width)
-    local i1 = rep(" ", width or 4)
+    local i1 = (" "):rep(width or 4)
     return setmetatable({[0] = "", [1] = i1}, Indent)
 end
