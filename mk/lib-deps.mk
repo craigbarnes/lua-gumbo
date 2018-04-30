616b886e644a905874f9873e1450708c909baeca
Craig Barnes <Cr@igBarn.es>
2018-04-30 01:05:25 +0100

    Move header dependency list from mk/lib.mk to mk/lib-deps.mk
    Also add a make target for regenerating it, instead of a hacky
    shell snippet.

---
+++ mk/lib-deps.mk
@@ -0,0 +1,28 @@
+build/lib/attribute.o: lib/attribute.c lib/attribute.h lib/gumbo.h lib/util.h
+build/lib/error.o: lib/error.c lib/error.h lib/gumbo.h lib/insertion_mode.h \
+ lib/string_buffer.h lib/token_type.h lib/parser.h lib/util.h \
+ lib/vector.h
+build/lib/string_buffer.o: lib/string_buffer.c lib/string_buffer.h lib/gumbo.h \
+ lib/string_piece.h lib/util.h
+build/lib/tag.o: lib/tag.c lib/gumbo.h lib/util.h lib/tag_lookup.h
+build/lib/utf8.o: lib/utf8.c lib/utf8.h lib/gumbo.h lib/error.h \
+ lib/insertion_mode.h lib/string_buffer.h lib/token_type.h lib/parser.h \
+ lib/util.h lib/vector.h
+build/lib/vector.o: lib/vector.c lib/vector.h lib/gumbo.h lib/util.h
+build/lib/char_ref.o: lib/char_ref.c lib/char_ref.h lib/error.h lib/gumbo.h \
+ lib/insertion_mode.h lib/string_buffer.h lib/token_type.h \
+ lib/string_piece.h lib/utf8.h lib/util.h
+build/lib/parser.o: lib/parser.c lib/attribute.h lib/gumbo.h lib/error.h \
+ lib/insertion_mode.h lib/string_buffer.h lib/token_type.h lib/parser.h \
+ lib/tokenizer.h lib/tokenizer_states.h lib/utf8.h lib/util.h \
+ lib/vector.h lib/replacement.h
+build/lib/string_piece.o: lib/string_piece.c lib/string_piece.h lib/gumbo.h \
+ lib/util.h
+build/lib/tokenizer.o: lib/tokenizer.c lib/tokenizer.h lib/gumbo.h lib/token_type.h \
+ lib/tokenizer_states.h lib/attribute.h lib/char_ref.h lib/error.h \
+ lib/insertion_mode.h lib/string_buffer.h lib/parser.h lib/string_piece.h \
+ lib/utf8.h lib/util.h lib/vector.h
+build/lib/util.o: lib/util.c lib/util.h lib/gumbo.h lib/parser.h
+build/lib/svg_attrs.o: lib/svg_attrs.c lib/replacement.h
+build/lib/svg_tags.o: lib/svg_tags.c lib/replacement.h
+build/lib/tag_lookup.o: lib/tag_lookup.c lib/tag_lookup.h lib/gumbo.h
--- mk/lib.mk
+++ mk/lib.mk
@@ -12,6 +12,8 @@ LIBGUMBO_OBJ = $(call PREFIX_OBJ, build/lib/, \
     string_piece tokenizer util ) \
     $(LIBGUMBO_OBJ_GPERF)
 
+LIBGUMBO_SRC = $(patsubst build/lib/%.o,lib/%.c, $(LIBGUMBO_OBJ))
+
 TEST_OBJ = $(call PREFIX_OBJ, build/lib/test_, \
     attribute char_ref parser string_buffer string_piece test_utils \
     tokenizer utf8 vector )
@@ -59,36 +61,11 @@ gperf-gen:
 	$(call GPERF_GEN, lib/svg_tags.c)
 	$(call GPERF_GEN, lib/svg_attrs.c)
 
+lib-deps-gen:
+	$(CC) -MM $(LIBGUMBO_SRC) | \
+	  sed 's|^\([^: ]\+:\)|build/lib/\1|' > mk/lib-deps.mk
 
-.PHONY: ragel-gen gperf-gen check-lib benchmark
 
-# sed -i '/^# sed/,$ { /^# sed/b; /^  gcc/b; d }' mk/lib.mk && \
-  gcc -MM lib/*.c | sed 's|^\([^: ]\+:\)|build/lib/\1|' >> mk/lib.mk
-build/lib/attribute.o: lib/attribute.c lib/attribute.h lib/gumbo.h lib/util.h
-build/lib/char_ref.o: lib/char_ref.c lib/char_ref.h lib/error.h lib/gumbo.h \
- lib/insertion_mode.h lib/string_buffer.h lib/token_type.h \
- lib/string_piece.h lib/utf8.h lib/util.h
-build/lib/error.o: lib/error.c lib/error.h lib/gumbo.h lib/insertion_mode.h \
- lib/string_buffer.h lib/token_type.h lib/parser.h lib/util.h \
- lib/vector.h
-build/lib/parser.o: lib/parser.c lib/attribute.h lib/gumbo.h lib/error.h \
- lib/insertion_mode.h lib/string_buffer.h lib/token_type.h lib/parser.h \
- lib/tokenizer.h lib/tokenizer_states.h lib/utf8.h lib/util.h \
- lib/vector.h lib/replacement.h
-build/lib/string_buffer.o: lib/string_buffer.c lib/string_buffer.h lib/gumbo.h \
- lib/string_piece.h lib/util.h
-build/lib/string_piece.o: lib/string_piece.c lib/string_piece.h lib/gumbo.h \
- lib/util.h
-build/lib/svg_attrs.o: lib/svg_attrs.c lib/replacement.h
-build/lib/svg_tags.o: lib/svg_tags.c lib/replacement.h
-build/lib/tag.o: lib/tag.c lib/gumbo.h lib/util.h lib/tag_lookup.h
-build/lib/tag_lookup.o: lib/tag_lookup.c lib/tag_lookup.h lib/gumbo.h
-build/lib/tokenizer.o: lib/tokenizer.c lib/tokenizer.h lib/gumbo.h lib/token_type.h \
- lib/tokenizer_states.h lib/attribute.h lib/char_ref.h lib/error.h \
- lib/insertion_mode.h lib/string_buffer.h lib/parser.h lib/string_piece.h \
- lib/utf8.h lib/util.h lib/vector.h
-build/lib/utf8.o: lib/utf8.c lib/utf8.h lib/gumbo.h lib/error.h \
- lib/insertion_mode.h lib/string_buffer.h lib/token_type.h lib/parser.h \
- lib/util.h lib/vector.h
-build/lib/util.o: lib/util.c lib/util.h lib/gumbo.h lib/parser.h
-build/lib/vector.o: lib/vector.c lib/vector.h lib/gumbo.h lib/util.h
+.PHONY: ragel-gen gperf-gen lib-deps-gen check-lib benchmark
+
+include mk/lib-deps.mk
