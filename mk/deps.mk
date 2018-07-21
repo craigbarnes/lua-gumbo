c8b602a8dccb424ccd388a0b3e64aedf336c19c3
Craig Barnes <Cr@igBarn.es>
2018-07-21 13:07:51 +0100

    Update mk/deps.mk

--- mk/deps.mk
+++ mk/deps.mk
@@ -1,5 +1,6 @@
-build/lib/attribute.o: lib/attribute.c lib/attribute.h lib/gumbo.h lib/util.h \
- lib/macros.h
+build/lib/ascii.o: lib/ascii.c lib/ascii.h lib/macros.h
+build/lib/attribute.o: lib/attribute.c lib/attribute.h lib/gumbo.h lib/ascii.h \
+ lib/macros.h lib/util.h
 build/lib/error.o: lib/error.c lib/error.h lib/gumbo.h lib/insertion_mode.h \
  lib/string_buffer.h lib/token_type.h lib/macros.h lib/parser.h \
  lib/util.h lib/vector.h
@@ -8,16 +9,16 @@ build/lib/string_buffer.o: lib/string_buffer.c lib/string_buffer.h lib/gumbo.h \
 build/lib/tag.o: lib/tag.c lib/gumbo.h lib/util.h lib/macros.h lib/tag_lookup.h
 build/lib/utf8.o: lib/utf8.c lib/utf8.h lib/gumbo.h lib/macros.h lib/error.h \
  lib/insertion_mode.h lib/string_buffer.h lib/token_type.h lib/parser.h \
- lib/util.h lib/vector.h
+ lib/ascii.h lib/vector.h
 build/lib/vector.o: lib/vector.c lib/vector.h lib/gumbo.h lib/util.h lib/macros.h
 build/lib/char_ref.o: lib/char_ref.c lib/char_ref.h lib/error.h lib/gumbo.h \
  lib/insertion_mode.h lib/string_buffer.h lib/token_type.h lib/macros.h \
  lib/utf8.h
-build/lib/parser.o: lib/parser.c lib/attribute.h lib/gumbo.h lib/error.h \
- lib/insertion_mode.h lib/string_buffer.h lib/token_type.h lib/macros.h \
- lib/parser.h lib/replacement.h lib/tokenizer.h lib/tokenizer_states.h \
- lib/utf8.h lib/util.h lib/vector.h
-build/lib/string_piece.o: lib/string_piece.c lib/gumbo.h lib/util.h lib/macros.h
+build/lib/parser.o: lib/parser.c lib/ascii.h lib/macros.h lib/attribute.h \
+ lib/gumbo.h lib/error.h lib/insertion_mode.h lib/string_buffer.h \
+ lib/token_type.h lib/parser.h lib/replacement.h lib/tokenizer.h \
+ lib/tokenizer_states.h lib/utf8.h lib/util.h lib/vector.h
+build/lib/string_piece.o: lib/string_piece.c lib/gumbo.h lib/ascii.h lib/macros.h
 build/lib/tokenizer.o: lib/tokenizer.c lib/tokenizer.h lib/gumbo.h lib/token_type.h \
  lib/tokenizer_states.h lib/attribute.h lib/char_ref.h lib/error.h \
  lib/insertion_mode.h lib/string_buffer.h lib/parser.h lib/utf8.h \
@@ -26,11 +27,11 @@ build/lib/util.o: lib/util.c lib/util.h lib/macros.h lib/gumbo.h
 build/lib/foreign_attrs.o: lib/foreign_attrs.c lib/replacement.h lib/gumbo.h \
  lib/macros.h
 build/lib/svg_attrs.o: lib/svg_attrs.c lib/replacement.h lib/gumbo.h lib/macros.h \
- lib/util.h
+ lib/ascii.h
 build/lib/svg_tags.o: lib/svg_tags.c lib/replacement.h lib/gumbo.h lib/macros.h \
- lib/util.h
+ lib/ascii.h
 build/lib/tag_lookup.o: lib/tag_lookup.c lib/tag_lookup.h lib/gumbo.h lib/macros.h \
- lib/util.h
+ lib/ascii.h
 
 build/lib/test_attribute.o: test/parser/attribute.cc lib/attribute.h lib/gumbo.h \
  test/parser/test_utils.h lib/gumbo.h lib/parser.h lib/vector.h
