fa1845343f568a3d7d95faa62754c3bc40aa7688
Craig Barnes <Cr@igBarn.es>
2018-08-19 21:07:58 +0100

    Add doc comments for GumboOutputStatus enum

--- lib/gumbo.h
+++ lib/gumbo.h
@@ -730,10 +730,29 @@ typedef struct GumboOptions {
 /** Default options struct; use this with gumbo_parse_with_options. */
 extern const GumboOptions kGumboDefaultOptions;
 
+/**
+ * Status code indicating whether parsing finished successfully or
+ * was stopped mid-document due to exceptional circumstances.
+ */
 typedef enum {
+  /**
+   * Indicates that parsing completed successfuly. The resulting tree
+   * will be a complete document.
+   */
   GUMBO_STATUS_OK,
+
+  /**
+   * Indicates that the maximum element nesting limit of 400 was
+   * reached during parsing. The resulting tree will be a partial
+   * document, with no further nodes created after the point where
+   * the limit was reached. The partial document may be useful for
+   * constructing an error message but typically shouldn't be used
+   * for other purposes.
+   */
+  GUMBO_STATUS_TREE_TOO_DEEP,
+
+  // Currently unused
   GUMBO_STATUS_OUT_OF_MEMORY,
-  GUMBO_STATUS_TREE_TOO_DEEP
 } GumboOutputStatus;
 
 
