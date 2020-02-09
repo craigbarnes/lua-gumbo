bacef46adc48bdcd3c738b05a6131e2ead17f2bf
Craig Barnes <Cr@igBarn.es>
2020-02-09 11:39:26 +0000

    Automatically add "-r -Otarget" to MAKEFLAGS

--- mk/util.mk
+++ mk/util.mk
@@ -2,10 +2,11 @@ streq = $(and $(findstring $(1),$(2)),$(findstring $(2),$(1)))
 try-run = $(if $(shell $(1) >/dev/null 2>&1 && echo 1),$(2),$(3))
 cc-option = $(call try-run,$(CC) $(1) -Werror -c -x c -o /dev/null /dev/null,$(1),$(2))
 
+MAKEFLAGS += -r -Otarget
 KERNEL = $(shell uname -s)
 DISTRO = $(shell . /etc/os-release && echo "$$NAME $$VERSION_ID")
 ARCH = $(shell uname -m)
-NPROC = $(shell sh mk/nproc.sh)
+NPROC = $(or $(shell sh mk/nproc.sh), 1)
 CC_VERSION = $(shell $(CC) --version 2>/dev/null | head -n1)
 MAKE_S = $(findstring s,$(firstword -$(MAKEFLAGS)))$(filter -s,$(MAKEFLAGS))
 XARGS_P = $(call try-run, printf "1\n2" | xargs -P2 -I@ echo '@', -P$(NPROC))
@@ -13,7 +14,6 @@ PRINTVAR = printf '\033[1m%15s\033[0m = %s$(2)\n' '$(1)' '$(strip $($(1)))' $(3)
 PRINTVARX = $(call PRINTVAR,$(1), \033[32m(%s)\033[0m, '$(origin $(1))')
 USERVARS = CC CFLAGS LDFLAGS
 LUAVARS = LUAS_FOUND LUA53_PC LUA52_PC LUA51_PC LUA53 LUA52 LUA51
-
 
 AUTOVARS = \
     KERNEL \
