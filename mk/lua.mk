ef9f5a9a5a529e3d7313c577bdca87f6eb6fdcf1
Craig Barnes <Cr@igBarn.es>
2020-07-09 22:14:06 +0100

    Add Lua 5.4 support to mk/lua.mk

--- mk/lua.mk
+++ mk/lua.mk
@@ -5,7 +5,7 @@ PKGCONFIG = $(or \
 
 $(call make-lazy,PKGCONFIG)
 
-LUA_VERSIONS = 5.3 5.2 5.1
+LUA_VERSIONS = 5.4 5.3 5.2 5.1
 LUA_SUFFIXES = $(subst .,,$(LUA_VERSIONS))
 PKGEXISTS = $(PKGCONFIG) --exists $(1) && echo $(1)
 PKGFIND = $(shell for P in $(1); do $(call PKGEXISTS, $$P) && break; done)
@@ -22,6 +22,11 @@ GET_LMOD_DIR = $(or \
 GET_CMOD_DIR = $(or \
     $(call LVAR, INSTALL_CMOD, $(1)), \
     $(strip $(call LVAR, libdir, $(1)))/lua/$(strip $(2)) \
+)
+
+LUA54_PC ?= $(or \
+    $(call PKGFIND, lua54 lua5.4 lua-5.4), \
+    $(call PKGMATCH, lua, >= 5.4, < 5.5) \
 )
 
 LUA53_PC ?= $(or \
@@ -43,21 +48,24 @@ LUA51_PC ?= $(or \
 $(foreach S, $(LUA_SUFFIXES), $(call make-lazy,LUA$(S)_PC))
 
 LUAS_FOUND = $(foreach S, $(LUA_SUFFIXES), $(if $(LUA$(S)_PC), lua$(S)))
-
 $(call make-lazy,LUAS_FOUND)
 
+LUA54_CFLAGS ?= $(call LFLAG, --cflags, 54)
 LUA53_CFLAGS ?= $(call LFLAG, --cflags, 53)
 LUA52_CFLAGS ?= $(call LFLAG, --cflags, 52)
 LUA51_CFLAGS ?= $(call LFLAG, --cflags, 51)
 
+LUA54_LMODDIR ?= $(call GET_LMOD_DIR, 54, 5.4)
 LUA53_LMODDIR ?= $(call GET_LMOD_DIR, 53, 5.3)
 LUA52_LMODDIR ?= $(call GET_LMOD_DIR, 52, 5.2)
 LUA51_LMODDIR ?= $(call GET_LMOD_DIR, 51, 5.1)
 
+LUA54_CMODDIR ?= $(call GET_CMOD_DIR, 54, 5.4)
 LUA53_CMODDIR ?= $(call GET_CMOD_DIR, 53, 5.3)
 LUA52_CMODDIR ?= $(call GET_CMOD_DIR, 52, 5.2)
 LUA51_CMODDIR ?= $(call GET_CMOD_DIR, 51, 5.1)
 
+LUA54 ?= $(call CMDFIND, $(LUA54_PC) lua5.4 lua-5.4 lua54)
 LUA53 ?= $(call CMDFIND, $(LUA53_PC) lua5.3 lua-5.3 lua53)
 LUA52 ?= $(call CMDFIND, $(LUA52_PC) lua5.2 lua-5.2 lua52)
 LUA51 ?= $(call CMDFIND, $(LUA51_PC) lua5.1 lua-5.1 lua51)
