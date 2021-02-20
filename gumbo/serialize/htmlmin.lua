4ec8e0359dc76ba718950f3812e9565b71f30374
Craig Barnes <craigbarnes@protonmail.com>
2021-02-20 19:25:15 +0000

    Improve handling of whitespace in "inline" elements, in htmlmin.lua

--- gumbo/serialize/htmlmin.lua
+++ gumbo/serialize/htmlmin.lua
@@ -2,12 +2,23 @@
 -- TODO: Add test cases
 
 local Buffer = require "gumbo.Buffer"
+local Set = require "gumbo.Set"
 local util = require "gumbo.util"
 local constants = require "gumbo.constants"
 local voidElements = constants.voidElements
 local rcdataElements = constants.rcdataElements
 local trimAndCollapseWhitespace = util.trimAndCollapseWhitespace
 local _ENV = nil
+
+-- These elements are "display: inline" by default
+-- (https://developer.mozilla.org/en-US/docs/Web/HTML/Inline_elements#elements)
+local inlineElements = Set [[
+    a abbr acronym audio b bdi bdo big br button canvas cite code data
+    datalist del dfn em embed i iframe img input ins kbd label map mark
+    meter noscript object output picture progress q ruby s samp script
+    select slot small span strong sub sup svg template textarea time tt
+    u var video wbr
+]]
 
 return function(node, buffer)
     local buf = buffer or Buffer()
@@ -37,6 +48,11 @@ return function(node, buffer)
                 buf:write(node.data)
             else
                 buf:write(trimAndCollapseWhitespace(node.escapedData))
+            end
+        elseif type == "whitespace" then
+            local parent = node.parentNode
+            if parent and inlineElements[parent.localName] then
+                buf:write(" ")
             end
         elseif type == "document" then
             if node.doctype then
