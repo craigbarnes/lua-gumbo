02438624b065130e9a88d68b312f155521949bb0
Craig Barnes <craigbarnes@protonmail.com>
2024-03-23 00:03:14 +0000

    Remove `make check-dist` target
    The checksums changed again, as occasionally happens with git-archive(1)
    generated tarballs. Stable release tarballs have been hosted elsewhere
    for a long time already, so this makefile target (and the sha1sum file)
    served no real purpose anyway.

--- mk/dist.mk
+++ mk/dist.mk
@@ -2,9 +2,6 @@ TAGS = 0.5 0.4 0.3 0.2 0.1
 
 dist: $(addprefix public/dist/lua-gumbo-, $(addsuffix .tar.gz, $(TAGS)))
 
-check-dist: dist
-	sha1sum -c test/dist-sha1sums.txt
-
 public/dist/lua-gumbo-%.tar.gz: | public/dist/
 	$(E) ARCHIVE '$@'
 	$(Q) git archive --prefix=lua-gumbo-$*/ -o $@ $*
@@ -13,4 +10,4 @@ public/dist/: | public/
 	@$(MKDIR) $@
 
 
-.PHONY: dist check-dist
+.PHONY: dist
